56172b4d25e7504d114fda3f88e3cb7b165c2f5a
tomoe <593575498@qq.com>
2020-12-22 23:48:35 +0800

    练习

---
+++ nonLinear.py
@@ -0,0 +1,15 @@
+#nonLinear.py
+from scipy.optimize import fsolve
+from math import sin,cos
+def f(x):
+    x0 = float(x[0])
+    x1 = float(x[1])
+    x2 = float(x[2])
+    return [
+        5*x1+3,
+        4*x0*x0 - 2*sin(x1*x2),
+        x1*x2 - 1.5]
+
+result = fsolve(f, [1,1,1])
+print(result)
+print(f(result))
