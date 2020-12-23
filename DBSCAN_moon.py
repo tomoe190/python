e89fb2646020e5d4915805d88e9a433c39644f8a
tomoe <593575498@qq.com>
2020-12-23 23:00:12 +0800

    修改

--- DBSCAN_moon.py
+++ DBSCAN_moon.py
@@ -11,16 +11,6 @@ plt.figure(figsize=(12, 9))
 plt.scatter(X[:, 0], X[:, 1], s=4, marker='o')
 plt.show()
 
-#%% 首先看看K-Means的聚类效果
-from sklearn.cluster import KMeans
-y_pred = KMeans(n_clusters=3, random_state=9).fit_predict(X)
-plt.figure(figsize=(12, 9))
-plt.scatter(X[:, 0], X[:, 1],s=4, c=y_pred)
-plt.title('k-means:k=3')
-plt.show()
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, y_pred))
-
 #%% 那么如果使用DBSCAN效果如何呢？我们先不调参，直接用默认参数，看看聚类效果,：
 from sklearn.cluster import DBSCAN
 y_pred = DBSCAN().fit_predict(X)     #eps=0.5, min_samples=5
